78ac9904e5a78009689220955a801b4be6aad379
Mohib <mohibullahmohim2020@gmail.com>
2024-02-03 19:59:06 +0600

    commit korlam arki

---
+++ app2.js
@@ -0,0 +1,16 @@
+
+/**
+ * Task-1:
+ * Count how many times a string has the letter a
+ * 
+ */
+
+let sentence1 = 'kotha rakhe ni boruna';
+document.write( '<br> kotha == '+ typeof(sentence1) );
+let make_array = sentence1.split('');
+console.log(' <br> made array to this sentence :  ' + make_array);
+document.write(' <br> made array to this sentence :  ' + make_array);
+
+let is_array = Array.isArray(make_array);
+document.write('<br> '+is_array)
+
