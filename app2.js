e7716d122cbfca87515b1af4b101bdde9cefe6f7
Mohib <mohibullahmohim2020@gmail.com>
2024-02-03 21:45:27 +0600

    boruna kotha rakhe ni

--- app2.js
+++ app2.js
@@ -12,5 +12,15 @@ console.log(' <br> made array to this sentence :  ' + make_array);
 document.write(' <br> made array to this sentence :  ' + make_array);
 
 let is_array = Array.isArray(make_array);
-document.write('<br> '+is_array)
+document.write('<br> is it an array : '+is_array)
 
+
+let letter_a = 'a';
+let sum = 0;
+for (let i = 0; i < make_array.length; i++){
+    let x = make_array[i];
+    if( x == 'a'){ 
+        sum++;
+    }
+}
+document.write( '<br> kotha rakhe ni boruna. Here a comes ' + sum + ' times. <br>');
